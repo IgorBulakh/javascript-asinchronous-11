e6698b40f154c478720826a013a111008e164c9c
IgorBulakh <bulach.igor@gmail.com>
2021-12-14 21:36:10 +0000

    Deploying to gh-pages from @ IgorBulakh/javascript-asinchronous-11@b98a8abe9af5494b0b9cbaff07224d693b767781 🚀

--- src.73d1d16a.js
+++ src.73d1d16a.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
+},{}],"d6sW":[function(require,module,exports) {
+const t={startRef:document.querySelector(".btn__start"),stopRef:document.querySelector(".btn__stop"),addColor:document.querySelector("body")},e=["#FFFFFF","#2196F3","#4CAF50","#FF9800","#009688","#795548"],o=0,r=e.length,s=(t,e)=>Math.floor(Math.random()*(e-t+1)+t),n={intervalId:null,isActive:!1,start(){this.isActive||(this.isActive=!0,this.intervalId=setInterval(()=>{t.addColor.style.backgroundColor=e[s(0,r)],console.log(Date.now())},1e3))},stop(){this.isActive=!1,clearInterval(this.intervalId)}};t.startRef.addEventListener("click",n.start.bind(n)),t.stopRef.addEventListener("click",n.stop.bind(n));
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.1fa81bff.js.map
+"use strict";require("./sass/main.scss"),require("./js/main.js");
+},{"./sass/main.scss":"clu1","./js/main.js":"d6sW"}]},{},["Focm"], null)
+//# sourceMappingURL=/parcel-project-template/src.73d1d16a.js.map
